db6cdaff6fd3b92eae64852b044a18a081d226b1
Arthur Moraes <arthur.m.thurler@gmail.com>
2026-05-11 20:49:28 -0300

    Adicinando mais verificações para a query de post, criando query de get e delete com suas devidas vertificações

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,13 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/todos", async (req, res) => {
-  const { title } = req.body;
+  const { title, done } = req.body;
+
+  if (done !== undefined && typeof done !== "boolean") {
+    return res.status(400).json({
+      message: "O campo done precisa ser true ou false",
+    });
+  }
 
   if (!title) {
     return res.status(400).json({
@@ -33,10 +39,24 @@ app.post("/todos", async (req, res) => {
     });
   }
 
+  const todoExist = await prisma.todo.findFirst({
+    where: {
+      title: title.trim(),
+      done
+    },
+  });
+
+  if (todoExist) {
+    return res.status(400).json({
+      message: "Esta tarefa já existe",
+    });
+  }
+
   try {
     const todo = await prisma.todo.create({
       data: {
         title: title.trim(),
+        done
       },
     });
 
@@ -46,6 +66,59 @@ app.post("/todos", async (req, res) => {
 
     return res.status(500).json({
       message: "Erro ao criar tarefa",
+    });
+  }
+});
+
+app.get("/todos", async (_, res) => {
+  try {
+    const allTodos = await prisma.todo.findMany({
+      orderBy: {
+        title: "asc",
+      },
+    });
+
+    return res.status(200).json(allTodos);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Erro ao buscar tarefas",
+    });
+  }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: "ID inválido",
+    });
+  }
+
+  const todoExists = await prisma.todo.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!todoExists) {
+    return res.status(404).json({
+      message: "Tarefa não encontrada",
+    });
+  }
+  try {
+    const deleteTodo = await prisma.todo.delete({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(200).json(deleteTodo);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Não foi possivel deletar esta tarefa",
     });
   }
 });
